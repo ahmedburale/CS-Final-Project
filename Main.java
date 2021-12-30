ecbab41f01e9a4bd46d07849495b659c4d2f0d52
Ahmed Burale <ahmedburale@users.noreply.github.com>
2021-12-29 18:30:44 -0700

    Main Class

---
+++ Main.java
@@ -0,0 +1,17 @@
+package com.AHMED;
+
+import com.AHMED.Screens.LogIn;
+import com.AHMED.Screens.MenuScreen;
+import com.AHMED.Screens.ProductsScreen;
+import com.AHMED.Screens.ReportScreen;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        new ProductsScreen();
+        new LogIn();
+        new MenuScreen();
+        new ReportScreen();
+    }
+}
